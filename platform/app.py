a5a4564f1a89ed617a114af02c884753680a4dd7
Geoffrey Kip <g_kip@live.com>
2017-10-01 16:00:35 -0400

    getting elastic search to work with flask and printing out data as a test

--- platform/app.py
+++ platform/app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask.ext.elasticsearch import FlaskElasticsearch
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import *
+from flask import jsonify
 
 app = Flask(__name__)
-es = FlaskElasticsearch(app)
+
+#Initialize elasticsearch with flask app
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 @app.route('/')
 def index():
     return 'Data Detective App'
 
+#Test to see if data prints out
 @app.route('/elasticsearch')
-def get():
-    res = requests.get('http://localhost:9200')
-    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+def elasticsearch():
     s = Search(using=es)
     res = es.search(index="paindex", body={"query": {"match_all": {}}})
-    print(res)
+    return jsonify(res)
 
 
 if __name__ == '__main__':
