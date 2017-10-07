6d0494ab5bb35e49989619776f942b120322fcac
Geoffrey Kip <g_kip@live.com>
2017-10-07 14:52:05 -0400

    commit server.py

--- platform/server.py
+++ platform/server.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from flask.ext.elasticsearch import FlaskElasticsearch
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import *
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
 #Initialize elasticsearch with flask app
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+res = es.search(index="paindex", body={"query": {"match_all": {}}})
+
+COUNTIES_LIST= All_data['county'].unique()
+MEASURES_LIST= All_data[All_data.columns.difference(['county','date'])]
 
 @app.route('/')
 def index():
@@ -16,9 +20,15 @@ def index():
 #Test to see if data prints out
 @app.route('/elasticsearch')
 def elasticsearch():
-    s = Search(using=es)
-    res = es.search(index="paindex", body={"query": {"match_all": {}}})
-    return jsonify(res)
+    return jsonify(res),200
+
+@app.route('/counties/list')
+def counties():
+    return jsonify(counties_list),200
+
+@app.route('/measures/list')
+def measures():
+    return jsonify(measures_list),200
 
 
 if __name__ == '__main__':
