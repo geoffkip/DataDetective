e00ef14c7a1519b422546696a77a523fe3ccac8c
Geoffrey Kip <g_kip@live.com>
2017-10-14 17:17:11 -0400

    Commit python script to load data into postgresql without using csvs

--- scripts/python_postgresql_load.py
+++ scripts/python_postgresql_load.py
@@ -8,6 +8,19 @@ Created on Sun Oct  8 12:49:52 2017
 
 import json
 import pandas as pd
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+
+con = psycopg2.connect( host= hostname, user= username, password= password, database=database)
+cur = con.cursor()  
+engine = create_engine('postgresql://postgres:titans@35.196.134.129:5432/postgres')
+
+cur.execute("DROP TABLE IF EXISTS data_points")
+cur.execute("CREATE TABLE data_points(Id SERIAL, data_set_id integer, county text,\
+            date date, measures json)")
+query =  "INSERT INTO data_points (Id, data_set_id, county,date,measures) VALUES (%s, %s, %s,%s,%s);"
+con.commit()
 
 def without_keys(d, keys):
       return {x: d[x] for x in d if x not in keys}
@@ -52,11 +65,17 @@ with open("data/snap.json") as snap_data:
     
 #Create datasets for loading into postgressql
 Jobs_datasets= without_keys(PA_jobs,{"data"})
+Jobs_datasets["data_set_id"] = 1
 Trainings_datasets= without_keys(PA_trainings,{"data"})
+#Trainings_datasets["data_set_id"] = 2
 Medicaid_datasets= without_keys(PA_medicaid,{"data", "measures"})
+Medicaid_datasets["data_set_id"] = 2
 Corrections_datasets= without_keys(PA_corrections,{"data", "measures"})
+Corrections_datasets["data_set_id"]= 3
 Snap_datasets= without_keys(PA_snap,{"data"})
+Snap_datasets["data_set_id"]=4
 Prisons_datasets= without_keys(PA_prisons, {"data", "measures"})
+Prisons_datasets["data_set_id"]= 5
 
 #parse json files to create data points datasets for loading into postgresql    
 Jobs_data_points= parse_json(PA_jobs)
@@ -70,12 +89,19 @@ Corrections_data_points= parse_json(PA_corrections)
 Corrections_data_points["data_set_id"] = 4
 Snap_data_points= parse_json(PA_snap)
 Snap_data_points["data_set_id"] = 5
-                
+     
+#load these datasets into postgresql database   
+             
+Jobs_data_points.to_sql("data_points", engine, if_exists='replace')
+Medicaid_data_points.to_sql("data_points", engine, if_exists='replace')
+Prisons_data_points.to_sql("data_points", engine, if_exists='replace')
+Corrections_data_points.to_sql("data_points", engine, if_exists='replace')
+Snap_data_points.to_sql("data_points", engine, if_exists='replace')
+
+      
  #Export as csvs
 Jobs_data_points.to_csv("data/jobs_psql.csv", sep=',', encoding='utf-8',index=False)               
 Medicaid_data_points.to_csv("data/medicaid_psql.csv", sep=',', encoding='utf-8',index=False)
 Prisons_data_points.to_csv("data/prisons_psql.csv", sep=',', encoding='utf-8',index=False)
 Corrections_data_points.to_csv("data/corrections_psql.csv", sep=',', encoding='utf-8',index=False)
 Snap_data_points.to_csv("data/snap_psql.csv", sep=',', encoding='utf-8',index=False)
-
-#To do load these datasets into postgresql database
